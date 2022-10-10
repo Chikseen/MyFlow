4c2f6d7a4aebcaaabcca108f631314dc99c5144f
Tim Menzel <tim_menzel@t-online.de>
2022-10-10 15:06:05 +0200

    Live Debug done, date problem resolved

--- api/Services/Counter/CounterHandler.cs
+++ api/Services/Counter/CounterHandler.cs
@@ -21,53 +21,34 @@ public class CounterHandler
             WHERE user_id = '{user.authid}'
             ORDER BY id ASC;";
         List<List<String>> counter = DatabaseService.query(getCounter);
-        Console.WriteLine("1: " + counter);
 
         List<Counter> allCounter = new List<Counter>();
-        Console.WriteLine("2: " + allCounter);
         for (int i = 0; i < counter.Count; i++)
         {
-            Console.WriteLine("3: " + i);
-            Console.WriteLine("4: " + counter[i][0]);
             String getNumber = @$"SELECT counter_id, value, date, unit FROM numbers WHERE counter_id = '{counter[i][0]}' ORDER BY date DESC LIMIT 1;";
-            Console.WriteLine("5");
             List<List<String>> numbers = DatabaseService.query(getNumber);
-            Console.WriteLine("6: " + numbers);
             if (numbers.Count > 0)
             {
-                Console.WriteLine("6.0: " + numbers[0][2]);
-
                 DotEnv.Load();
                 String os = Environment.GetEnvironmentVariable("OS")!;
-                Console.WriteLine("6.1.0: " + os);
 
                 String[] datearr;
                 DateTime date;
-
                 if (os == "w")
                 {
-                    datearr = numbers[0][2].Split(" ")[0].Split("."); // Need to transform due how input is handelt by postgres
+                    datearr = numbers[0][2].Split(" ")[0].Split(".");
                     date = new DateTime(Int32.Parse(datearr[2]), Int32.Parse(datearr[1]), Int32.Parse(datearr[0]));
                 }
                 else
                 {
-                    datearr = numbers[0][2].Split(" ")[0].Split("/"); // Need to transform due how input is handelt by postgres
+                    datearr = numbers[0][2].Split(" ")[0].Split("/");
                     date = new DateTime(Int32.Parse(datearr[2]), Int32.Parse(datearr[0]), Int32.Parse(datearr[1]));
                 }
-
-                Console.WriteLine("6.1: " + datearr);
-                Console.WriteLine("datearr[2]: " + datearr[2]);
-                Console.WriteLine("datearr[1]: " + datearr[1]);
-                Console.WriteLine("datearr[0]: " + datearr[0]);
-                Console.WriteLine("6.2: " + date);
                 allCounter.Add(new Counter(Int32.Parse(counter[i][0]), counter[i][1], counter[i][2], DateTime.Parse(counter[i][3]), Int32.Parse(numbers[0][1]), date, numbers[0][3]));
-                Console.WriteLine("6.3");
             }
             else
                 allCounter.Add(new Counter(Int32.Parse(counter[i][0]), counter[i][1], counter[i][2], DateTime.Parse(counter[i][3]), -1, null, "-"));
-            Console.WriteLine("7");
         }
-        Console.WriteLine("8");
         return allCounter;
     }
 
