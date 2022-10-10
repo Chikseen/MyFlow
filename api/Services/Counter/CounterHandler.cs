de73fe06422b0d2b6646c415e41e01f818e5cab8
Tim Menzel <tim_menzel@t-online.de>
2022-10-10 14:50:29 +0200

    Live server numbers debug

--- api/Services/Counter/CounterHandler.cs
+++ api/Services/Counter/CounterHandler.cs
@@ -28,18 +28,24 @@ public class CounterHandler
             Console.WriteLine("3: " + i);
             Console.WriteLine("4: " + counter[i][0]);
             String getNumber = @$"SELECT counter_id, value, date, unit FROM numbers WHERE counter_id = '{counter[i][0]}' ORDER BY date DESC LIMIT 1;";
-            Console.WriteLine("5: ");
+            Console.WriteLine("5");
             List<List<String>> numbers = DatabaseService.query(getNumber);
             Console.WriteLine("6: " + numbers);
             if (numbers.Count > 0)
             {
+                Console.WriteLine("6.0: " + numbers);
                 String[] datearr = numbers[0][2].Split(" ")[0].Split("."); // Need to transform due how input is handelt by postgres
+                Console.WriteLine("6.1: " + datearr);
                 DateTime date = new DateTime(Int32.Parse(datearr[2]), Int32.Parse(datearr[1]), Int32.Parse(datearr[0]));
+                Console.WriteLine("6.2: " + date);
                 allCounter.Add(new Counter(Int32.Parse(counter[i][0]), counter[i][1], counter[i][2], DateTime.Parse(counter[i][3]), Int32.Parse(numbers[0][1]), date, numbers[0][3]));
+                Console.WriteLine("6.3");
             }
             else
                 allCounter.Add(new Counter(Int32.Parse(counter[i][0]), counter[i][1], counter[i][2], DateTime.Parse(counter[i][3]), -1, null, "-"));
+            Console.WriteLine("7");
         }
+        Console.WriteLine("8");
         return allCounter;
     }
 
