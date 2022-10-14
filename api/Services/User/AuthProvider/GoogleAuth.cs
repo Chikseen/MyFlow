492ed3969ce22e006826e6104623124577a74bb8
Tim Menzel <tim_menzel@t-online.de>
2022-10-14 11:43:34 +0200

    debug google live

--- api/Services/User/AuthProvider/GoogleAuth.cs
+++ api/Services/User/AuthProvider/GoogleAuth.cs
@@ -24,17 +24,25 @@ public class GoogleAuth
             { "grant_type", "authorization_code" },
             { "redirect_uri", "http://localhost:7085/auth/google" }
         };
+        Console.WriteLine("code: " + code);
         var content = new FormUrlEncodedContent(values);
         var response = await accessTokenClient.PostAsync("https://oauth2.googleapis.com/token", content);
         var responseString = await response.Content.ReadAsStringAsync();
+        Console.WriteLine("responseString: " + responseString);
+
 
         GoogleAT token = JsonConvert.DeserializeObject<GoogleAT>(responseString)!;
-        await getUserDataFromAT(token.access_token!);
-        return token.access_token!;
+        if (token.access_token == "")
+        {
+            await getUserDataFromAT(token.access_token!);
+            return token.access_token!;
+        }
+        else return "Eroor";
     }
 
     public async Task<User> getUserDataFromAT(String access_token)
     {
+        Console.WriteLine("access_token: " + access_token);
         UserHandler userHandler = new UserHandler();
         HttpClient userDataClient = new HttpClient();
         Console.WriteLine($"https://www.googleapis.com/oauth2/v1/userinfo?access_token={access_token}");
